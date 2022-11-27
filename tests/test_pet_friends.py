17437163ced8eec4ce1cd7cabef7497f2a4f9dc3
tzzzz11 <116031681+tzzzz11@users.noreply.github.com>
2022-11-27 21:21:18 +0300

    Create test_pet_friends.py

---
+++ tests/test_pet_friends.py
@@ -0,0 +1,53 @@
+from api import PetFriends
+from settings import valid_email, valid_password
+
+pf = PetFriends()
+
+def test_get_api_key_for_valid_user(email=valid_email, password=valid_password):
+    status, result = pf.get_api_key(email, password)
+    assert status == 200
+    assert 'key' in result
+
+def test_get_all_pets_with_valid_key(filter=''):
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.get_list_of_pets(auth_key, filter)
+    assert status == 200
+    assert len(result['pets']) > 0
+
+def test_add_new_pet_with_valid_data(name='Шарик1', animal_type='', age='4', pet_photo='images/123.jpg'):
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.add_new_pet(auth_key, name, animal_type, age, pet_photo)
+    assert status == 200
+    assert result['name'] == name
+
+def test_add_pet_photo(pet_photo='images/124.jpg'):
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_list_of_pets(auth_key, 'my_pets')
+    if len(my_pets['pets']) > 0:
+        status, result = pf.add_pet_photo(auth_key, my_pets['pets'][0]['id'], pet_photo)
+        assert status == 200
+    else:
+        raise Exception('No my pets')
+
+def test_successful_delete_self_pet():
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+    if len(my_pets['pets']) == 0:
+        pf.add_new_pet(auth_key, "Суперкот", "кот", "3", "images/cat1.jpg")
+        _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+    pet_id = my_pets['pets'][0]['id']
+    status, _ = pf.delete_pet(auth_key, pet_id)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+    assert status == 200
+    assert pet_id not in my_pets.values()
+
+def test_successful_update_self_pet_info(name='Мурзик', animal_type='Кот', age=5):
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+    if len(my_pets['pets']) > 0:
+        status, result = pf.update_pet_info(auth_key, my_pets['pets'][0]['id'], name, animal_type, age)
+        assert status == 200
+        assert result['name'] == name
+    else:
+        raise Exception("There is no my pets")
+    
